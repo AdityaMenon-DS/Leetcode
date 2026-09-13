272d7f27b8e5cda22e6ab13df5f6bf09b65ae9c4
AdityaMenon-DS <aditya.marath@gmail.com>
2026-09-13 23:51:53 +0530

    JS

---
+++ 55. Jump Game.js
@@ -0,0 +1,13 @@
+var canJump = function(nums) {
+    let far = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > far) {
+            return false;
+        }
+
+        far = Math.max(far, i + nums[i]);
+    }
+
+    return true;
+};
