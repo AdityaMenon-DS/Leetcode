5944879a47fe11174a09a43caa9beee7b2666b8f
AdityaMenon-DS <aditya.marath@gmail.com>
2026-09-21 23:21:22 +0530

    JS

---
+++ 97. Interleaving String.js
@@ -0,0 +1,28 @@
+var isInterleave = function(s1, s2, s3) {
+    if (s1.length + s2.length !== s3.length) {
+        return false;
+    }
+
+    let m = s1.length;
+    let n = s2.length;
+
+    let dp = Array.from({ length: m + 1 }, () =>
+        new Array(n + 1).fill(false)
+    );
+
+    dp[0][0] = true;
+
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i > 0 && s1[i - 1] === s3[i + j - 1]) {
+                dp[i][j] = dp[i][j] || dp[i - 1][j];
+            }
+
+            if (j > 0 && s2[j - 1] === s3[i + j - 1]) {
+                dp[i][j] = dp[i][j] || dp[i][j - 1];
+            }
+        }
+    }
+
+    return dp[m][n];
+};
